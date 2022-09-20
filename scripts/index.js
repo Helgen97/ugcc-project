493ca866d71c06387ce0b7ba7b70de394057a12f
Dmytro Donchenko <drmegman@gmail.com>
2022-09-20 19:59:27 +0300

    Made work with JS scripts

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,62 @@
-document.addEventListener('click', (ev) => {
-    if (ev.target.classList.contains("dropdown_title")) {
-        ev.target.parentNode.classList.toggle("show");
-    };
-})
+let modalOpenButton = $("#modal");
+let modalWindowContainer = $(".modal");
+let mobileMenuOpenButton = $("#mobile_menu");
+let mobileMenuContainer = $(".navigation_mobile");
+let mobileSearchOpenButton = $("#search_button");
+let mobileSearchContainer = $(".navigation_search");
+let mobileSearchInputContainer = $(".navigation_search_input-container");
+let mobileSearchInput = $("#mobile_search_input");
+let desktopSearchInput = $("#desktop_search_input");
+let desktopSearchButton = $("#desktop_search_button");
 
-$(document).on("click", (e) => {
-    if (e.target.classList.contains("dropdown_title")) {
-        e.target.classList.toggle("show")
-    }
-})
-
-
-$("#mobile_menu").on('click', (e) => {
-    $("#mobile_menu").parent().parent().addClass("show");
-})
-
-$(document).mouseup(function (e) {
-    var container = $(".navigation_mobile");
-    if (container.has(e.target).length === 0) {
-        container.removeClass("show");
-    }
+modalOpenButton.on("click", () => {
+    modalWindowContainer.addClass("show");
 });
 
-$("#search_button").on("click", () => {
-    $(".navigation_search_input-container").toggleClass("show");
+mobileMenuOpenButton.on("click", () => {
+    mobileMenuOpenButton.parent().parent().addClass("show");
+});
+
+mobileSearchOpenButton.on("click", () => {
+    mobileSearchInputContainer.toggleClass("show");
+});
+
+mobileSearchInput.on("keyup", (event) => {
+    if (event.key === "Enter") {
+        alert(event.target.value);
+    }
 })
 
-$("#modal").on("click", () => {
-    $(".modal").toggleClass("show");
+desktopSearchButton.on("click", () => {
+    alert(desktopSearchInput.val());
 })
 
-$(document).mouseup(function (e) {
-    var container = $(".modal");
-    if (container.has(e.target).length === 0 || e.target.classList.contains("close-btn")) {
-        container.removeClass("show");
+desktopSearchInput.on("keyup", (event) => {
+    if (event.key === "Enter") {
+        alert(event.target.value);
+    }
+})
+
+
+$(document).on('mouseup', (event) => {
+    let currentTarget = $(event.target);
+
+    if (
+        modalWindowContainer.has(currentTarget).length === 0 ||
+        currentTarget.hasClass("close-btn")
+    ) {
+        modalWindowContainer.removeClass("show");
+    }
+
+    if (mobileMenuContainer.has(currentTarget).length === 0) {
+        mobileMenuContainer.removeClass("show");
+    }
+
+    if (currentTarget.hasClass("dropdown_title")) {
+        currentTarget.parent().toggleClass("show");
+    }
+
+    if (mobileSearchContainer.has(currentTarget).length === 0) {
+        mobileSearchInputContainer.removeClass("show");
     }
 });
